7a8e806d526e1800738e54c5c0ec464eceed46b3
theik11 <ibrahim258159@gmail.com>
2026-09-01 15:56:09 +0600

    feat: initialize main entry point with Firebase, RevenueCat, and GetMaterialApp configuration

--- lib/main.dart
+++ lib/main.dart
@@ -114,7 +114,7 @@ class UtilScreenMobile extends StatelessWidget {
             },
             navigatorKey: NavigationService.navigatorKey,
             onGenerateRoute: RouteGenerator.generateRoute,
-            home: OnboardingScreen(), //
+            home: Loading(), //
           ),
         );
       },
